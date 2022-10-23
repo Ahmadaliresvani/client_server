dae36d204c1c1767c31973885583edb6e7f94a49
Padidar <aliabdol@gamil.com>
2022-10-23 21:28:37 +0330

    convert mt to Mt

--- tools/work_with_time.py
+++ tools/work_with_time.py
@@ -1,5 +1,5 @@
 import datetime
-import MetaTrader5 as mt
+import MetaTrader5 as Mt
 import os
 import pytz
 
@@ -16,37 +16,37 @@ def get_forex_time_naive():
 
 def convert_time_interval_metatrader_time(time_name):
     if time_name == '1m':
-        return mt.TIMEFRAME_M1
+        return Mt.TIMEFRAME_M1
     elif time_name == '5m':
-        return mt.TIMEFRAME_M5
+        return Mt.TIMEFRAME_M5
     elif time_name == '10m':
-        return mt.TIMEFRAME_M10
+        return Mt.TIMEFRAME_M10
     elif time_name == '15m':
-        return mt.TIMEFRAME_M15
+        return Mt.TIMEFRAME_M15
     elif time_name == '30m':
-        return mt.TIMEFRAME_M30
+        return Mt.TIMEFRAME_M30
     elif time_name == '1h':
-        return mt.TIMEFRAME_H1
+        return Mt.TIMEFRAME_H1
     elif time_name == '4h':
-        return mt.TIMEFRAME_H4
+        return Mt.TIMEFRAME_H4
     else:
         raise ValueError("time interval not allowed")
 
 
 def convert_metatrader_time_to_delta_time(meta_time):
-    if meta_time == mt.TIMEFRAME_M1:
+    if meta_time == Mt.TIMEFRAME_M1:
         return datetime.timedelta(minutes=1)
-    elif meta_time == mt.TIMEFRAME_M5:
+    elif meta_time == Mt.TIMEFRAME_M5:
         return datetime.timedelta(minutes=5)
-    elif meta_time == mt.TIMEFRAME_M10:
+    elif meta_time == Mt.TIMEFRAME_M10:
         return datetime.timedelta(minutes=10)
-    elif meta_time == mt.TIMEFRAME_M15:
+    elif meta_time == Mt.TIMEFRAME_M15:
         return datetime.timedelta(minutes=15)
-    elif meta_time == mt.TIMEFRAME_M30:
+    elif meta_time == Mt.TIMEFRAME_M30:
         return datetime.timedelta(minutes=30)
-    elif meta_time == mt.TIMEFRAME_H1:
+    elif meta_time == Mt.TIMEFRAME_H1:
         return datetime.timedelta(hours=1)
-    elif meta_time == mt.TIMEFRAME_H4:
+    elif meta_time == Mt.TIMEFRAME_H4:
         return datetime.timedelta(hours=4)
     else:
         raise ValueError("meta time interval not allowed")
